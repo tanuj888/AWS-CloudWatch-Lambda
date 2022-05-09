11513d56489747a7550a60da842d7a28409d5ee1
Tanuj Jain <tanuj888.com>
2022-05-09 13:44:48 +1000

    new

--- provider.tf
+++ provider.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+/*terraform init \
+    -backend-config="address=https://gitlab.com/api/v4/projects/35924451/terraform/state/AWS-CloudWatch-Lambda" \
+    -backend-config="lock_address=https://gitlab.com/api/v4/projects/35924451/terraform/state/AWS-CloudWatch-Lambda" \
+    -backend-config="unlock_address=https://gitlab.com/api/v4/projects/35924451/terraform/state/AWS-CloudWatch-Lambda" \
+    -backend-config="username=tanujjain" \
+    -backend-config="password=glpat-QJggChY3NtWBcDzUPrUo" \
+    -backend-config="lock_method=POST" \
+    -backend-config="unlock_method=DELETE" \
+    -backend-config="retry_wait_min=5"
 
 /*terraform {
     backend "s3" {
